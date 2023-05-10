8402b8984944ea3a56680f230b26e806bb78970a
Himanshuagarwal01 <himanshu.agarwal@deeporion.com>
2023-05-10 15:38:34 +0530

    programming

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -23,3 +23,8 @@ export default store;
 // A single store containing "global" state
 // Dispatching plain object actions to the store when something happens in the app
 // Pure reducer functions looking at those actions and returning immutably updated state
+
+
+// npm install @reduxjs/toolkit--- comand to install redux toolkit
+
+
